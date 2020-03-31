6e05f26b893079f1260123a73a0d8260a11ea6a4
Stefan Mihajlović <stefan.mihajlovic.012@gmail.com>
2020-03-31 17:21:06 +0200

    Enhance everything

--- create_coupon_yabby.py
+++ create_coupon_yabby.py
@@ -1,84 +1,33 @@
 import requests
+from random import choice
+from string import digits, ascii_letters
+import datetime as dt
 
-# api basics
-cert_path = r'certificates/mccyabby.pem' # need absolute pathing
-main_url = "https://admin.yabbycasino.com/YABBYECVSUGMOQMOIPQO/RTGWebAPI/"
+def create_coupon(login, reward):
+    # api basics
+    cert_path = r'certificates/mccyabby.pem' # need absolute pathing
+    main_url = "https://admin.yabbycasino.com/YABBYECVSUGMOQMOIPQO/RTGWebAPI/"
 
-payload = r"""{
-  "redeem_amount": 10,
-  "play_through_amount": 99999,
-  "can_be_referred": true,
-  "limit_withdrawals": "fixed_amount",
-  "limit_withdrawals_fixed_amount": 1,
-  "limit_withdrawals_multiple_deposit": 0,
-  "limit_withdrawals_code": "string",
-  "disable_all_net_progressives": true,
-  "earnings_restricted_to_deposit": false,
-  "free_spin_deposit_list": null,
-  "previous_coupon_id": 0,
-  "minimum_bonus_balance_type": "no_limit",
-  "minimum_bonus_balance": 0,
-  "require_deposit_fixed_amount_on_redemption": false,
-  "play_through_free_spin_wins": 0,
-  "wager_bonus": 0,
-  "free_spins_configuration": {
-  },
-  "coupon_code": "TEST-API-C12",
-  "date_valid_start": "2020-03-30T18:02:09.590Z",
-  "date_valid_end": "2020-03-31T18:02:09.590Z",
-  "user_id": 0,
-  "method_list": "string",
-  "redeem_zero": 0,
-  "redeem_verified_email": true,
-  "redeem_new_players": false,
-  "comment": "API Test",
-  "associated_affiliate_id": null,
-  "redeem_for_affiliate_id": null,
-  "schedule_days_of_week": null,
-  "schedule_start_time": 0,
-  "schedule_duration_minutes": 0,
-  "published": false,
-  "allow_redemption_from": [
-    "download"
-  ],
-  "allowed_skin_list": [
-  ],
-  "exclude_player_class": null,
-  "player_filters": {
-    "days_since_sign_up": {
-      "type": "string",
-      "value": {}
-    },
-    "days_since_birthday": {
-      "type": "string",
-      "value": {}
-    },
-    "days_since_first_deposit": {
-      "type": "string",
-      "value": {}
-    },
-    "days_since_last_deposit": {
-      "type": "string",
-      "value": {}
-    },
-    "countries_to_exclude": [
-    ],
-    "exclude_coupons": [
-    ],
-    "currencies": [
-      "USD",
-      "AUD",
-      "EUR",
-      "ZAR"
-    ]
-  },
-  "sim_percent": 0,
-  "count_days_deposit": 0,
-  "count_days_play": 0,
-  "game_play_through_list": [
-  ]
-}"""
+    # define payload
+    coupon_code = "CB-MON-"+"".join([choice(ascii_letters + digits) for n in range(7)]).upper() # create random coupon code
+    start_today = dt.date.today().strftime("%Y-%m-%dT%H:%M:%S.%fZ") # required time format
+    end_today = (dt.date.today()+dt.timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S.%fZ") # required time format
+    comment = f"Monday Cashback reward for {login} | 10x WAG | 10x MCO | All NP Games Allowed" # coupon comment
 
-#[
-#      "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"
-#  ]
+    payload={"redeem_amount":reward,"play_through_amount":reward*10,"can_be_referred":True,"limit_withdrawals":"fixed_amount","limit_withdrawals_fixed_amount":reward*10,"limit_withdrawals_multiple_deposit":0,"limit_withdrawals_code":"string","disable_all_net_progressives":True,"earnings_restricted_to_deposit":False,"free_spin_deposit_list":None,"previous_coupon_id":0,"minimum_bonus_balance_type":"no_limit","minimum_bonus_balance":0,"require_deposit_fixed_amount_on_redemption":False,"play_through_free_spin_wins":0,"wager_bonus":0,"free_spins_configuration":None,"coupon_code":coupon_code,"date_valid_start":start_today,"date_valid_end":end_today,"user_id":0,"method_list":"string","redeem_zero":0,"redeem_verified_email":True,"redeem_new_players":False,"comment":comment,"associated_affiliate_id":None,"redeem_for_affiliate_id":None,"schedule_days_of_week":None,"schedule_start_time":0,"schedule_duration_minutes":3599,"published":False,"allow_redemption_from":["download"],"allowed_skin_list":[],"exclude_player_class":None,"player_filters":{"days_since_sign_up":{"type":"string","value":{}},"days_since_birthday":{"type":"string","value":{}},"days_since_first_deposit":{"type":"string","value":{}},"days_since_last_deposit":{"type":"string","value":{}},"countries_to_exclude":[],"exclude_coupons":[],"currencies":["USD","AUD","EUR"]},"sim_percent":0,"count_days_deposit":0,"count_days_play":0,"game_play_through_list":[]}
+
+    print(f"-- creating coupon for {login}...")
+    while True: # attempt multiple times if coupon code exists
+        r=requests.post(url=main_url+"api/v2/coupons/bonus-balance/single-use/fixed-amount-on-redemption", json=payload, cert=cert_path)
+        if r.status_code == 200: 
+          print(f"---- success!")
+          return True # if successful, end function
+        elif r.json()['Status'] == "coupon_code_already_exists": # if coupon code exists
+            print(f"---- {coupon_code} already exists... generating new coupon code...")
+            coupon_code = "CB-MON-"+"".join([choice(ascii_letters + digits) for n in range(7)]).upper() # randomize new code
+            payload["coupon_code"] = coupon_code # input new coupon code
+            continue # repeat
+        else:
+          print(f"---- failed: {r.status_code}")
+          print(f"---- {r.text}")
+          return False # coupon creation failed
--- yabby_cb_mon.py
+++ yabby_cb_mon.py
@@ -1,98 +1,192 @@
+################### TERMS ###################
+min_loss = 33
+min_deposits = 0
+max_ratio = 100
+min_reward = 1
+################### TERMS ###################
+
 import requests
 import pandas as pd
 import datetime as dt
+from time import sleep
+from sys import exit
+from random import choice
+from string import digits, ascii_letters
 
 # dates and times
 today = dt.date.today()
 start_date = f"{(today - dt.timedelta(days=today.weekday()+7)).strftime('%m-%d-%y')} 00:00"
 end_date = f"{(today - dt.timedelta(days=today.weekday()+1)).strftime('%m-%d-%y')} 23:59"
-
 # api basics
 cert_path = r'certificates/mccyabby.pem' # need absolute pathing
 main_url = "https://admin.yabbycasino.com/YABBYECVSUGMOQMOIPQO/RTGWebAPI/"
 
-# terms
-min_loss = 33
-min_deposits = 0
-max_ratio = 100
-min_reward = 1
+def get(call, params, description):
+    print(f"-- get {description}...")
+    for n in range(3):
+        r = requests.get(url=main_url+call, params=params, cert=cert_path)
+        if r.status_code == 200:
+            print(f"----> success!")
+            return r.json()
+        else:
+            print(f"----> failed: {r.status_code}")
+            print(f"---- {r.text}")
+            if n == 2:
+                print(f"----> request failed: get {description}")
+                return False
+            print(f"-- get depositors attempt {n+1}...")
+
+def create_coupon(login, reward):
+    # define payload
+    # required time formats
+    start_today = dt.date.today().strftime("%Y-%m-%dT%H:%M:%S.%fZ") 
+    end_today = (dt.date.today()+dt.timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    
+    # necessary coupon details
+    coupon_code = "CB-MON-"+"".join([choice(ascii_letters + digits) for n in range(7)]).upper() 
+    comment = f"Monday Cashback reward for {login} | 10x WAG | 10x MCO | All NP Games Allowed" # coupon comment
+
+    payload={"redeem_amount":reward,"play_through_amount":reward*10,"can_be_referred":True,"limit_withdrawals":"fixed_amount","limit_withdrawals_fixed_amount":reward*10,"limit_withdrawals_multiple_deposit":0,"limit_withdrawals_code":"string","disable_all_net_progressives":True,"earnings_restricted_to_deposit":False,"free_spin_deposit_list":None,"previous_coupon_id":0,"minimum_bonus_balance_type":"no_limit","minimum_bonus_balance":0,"require_deposit_fixed_amount_on_redemption":False,"play_through_free_spin_wins":0,"wager_bonus":0,"free_spins_configuration":None,"coupon_code":coupon_code,"date_valid_start":start_today,"date_valid_end":end_today,"user_id":0,"method_list":"string","redeem_zero":0,"redeem_verified_email":True,"redeem_new_players":False,"comment":comment,"associated_affiliate_id":None,"redeem_for_affiliate_id":None,"schedule_days_of_week":None,"schedule_start_time":0,"schedule_duration_minutes":3599,"published":False,"allow_redemption_from":["download"],"allowed_skin_list":[],"exclude_player_class":None,"player_filters":{"days_since_sign_up":{"type":"string","value":{}},"days_since_birthday":{"type":"string","value":{}},"days_since_first_deposit":{"type":"string","value":{}},"days_since_last_deposit":{"type":"string","value":{}},"countries_to_exclude":[],"exclude_coupons":[],"currencies":["USD","AUD","EUR"]},"sim_percent":0,"count_days_deposit":0,"count_days_play":0,"game_play_through_list":[]}
+
+    while True: # attempt multiple times if coupon code exists
+        print(f"-- create {coupon_code}...")
+        r=requests.post(url=main_url+"api/v2/coupons/bonus-balance/single-use/fixed-amount-on-redemption", json=payload, cert=cert_path)
+        if r.status_code == 200: 
+          print(f"----> success!")
+          return coupon_code # if successful, end function
+        elif r.json()['Status'] == "coupon_code_already_exists": # if coupon code exists
+            print(f"----> {coupon_code} already exists. generating new coupon code...")
+            coupon_code = "CB-MON-"+"".join([choice(ascii_letters + digits) for n in range(7)]).upper() # randomize new code
+            payload["coupon_code"] = coupon_code # input new coupon code
+            continue # repeat
+        else:
+          print(f"----> failed: {r.status_code}")
+          print(f"---- {r.text}")
+          return False # coupon creation failed
+
+def redeem_coupon(pid, coupon_code):
+    balance = get(f"players/{pid}/balance",{'forMoney':'True'}, "active balance") # check active balance
+    if balance == False:
+        return 0
+    elif balance['balance'] >= 1:
+        print("----> player has active balance")
+    print(f"-- redeem {coupon_code}...")
+    r = requests.post(url=main_url+f'v2/players/{pid}/coupons',params={'couponCode':coupon_code},cert=cert_path)
+    if r.status_code == 200:
+        print("----> success!")
+        return True
+    if r.json()['Status'] == 'player_excluded_from_redeeming_all_coupons':
+        print("----> excluded from coupons")
+        return False
+    elif r.json()['Status'] == 'previous_coupon_pending':
+        print("----> previous coupon pending")
+        return 0
+    else:
+        print(f"----> failed: {r.status_code}")
+        print(f"---- {r.text}")
+        return 0
+
+def place_comment():
+    pass
+
+def send_email(status):
+    pass
 
 # initiation prompt
-print("Monday Cashback Rewards Module started:")
-print("==========================")
+print("\nMonday Cashback Rewards Module started:")
+terms=["====================================================", "Terms setup:", f"Minimum Loss: ${min_loss}", f"Minimum Deposits: ${min_deposits}", f"Minimum Reward: ${min_reward}", f"Maximum WDs/DEPs Ratio: {max_ratio}%"]
+for item in terms:
+    print(item)
 
-if input("Skip processing rewards (for testing purposes)? [Y/N]: ") == 'Y'.casefold():
+# for testing purposes
+if input("Skip processing rewards (for testing purposes)? [Y/N]: ") == 'N'.casefold(): # process rewards
     testing = False
     print("NOT A DRILL - REWARDS WILL BE PROCESSED!")
-else:
+    if input("Proceed? [Y/N]: ") == 'N'.casefold(): # double check
+        testing = True
+        print("Testing mode ON - rewards WILL NOT be processed.")
+else: # testing mode
     testing = True
     print("Testing mode ON - rewards WILL NOT be processed.")
-
-print("Terms setup:")
-print(f"Minimum Loss: ${min_loss}")
-print(f"Minimum Deposits: ${min_deposits}")
-print(f"Maximum WDs/DEPs Ratio: {max_ratio}%")
-print(f"Minimum Reward: ${min_reward}")
-print("==========================")
+print("====================================================")
 
 # get depositors for last week
-print(f"Getting depositors from {start_date} to {end_date}...")
-r = requests.get(url=main_url+"api/reports/depositors", params={'startDate':start_date, 'endDate':end_date}, cert=cert_path)
-
-df1 = pd.DataFrame(r.json()) # make a dataframe from received list of dictionaries
-df1 = df1.drop(['casino_name', 'last_name', 'day_phone_number', 'evening_phone_number', 'full_address', 'address_2', 'city', 'country', 'zip_code', 'net_cash_position', 'signup_date', 'signup_skin', 'last_game_date', 'birth_date'], axis=1) # drop unnecessary columns
+print(f"-- get depositors report [{start_date} to {end_date}]...")
+for n in range(5): # attempt 5 times
+    r = requests.get(url=main_url+"api/reports/depositors", params={'startDate':start_date, 'endDate':end_date}, cert=cert_path)
+    if r.status_code == 200:
+        total = len(r.json()) # get depositor count
+        df1 = pd.DataFrame(r.json()) # make a dataframe from received list of dictionaries
+        df1 = df1.drop(['casino_name', 'last_name', 'day_phone_number', 'evening_phone_number', 'full_address', 'address_2', 'city', 'country', 'zip_code', 'net_cash_position', 'signup_date', 'signup_skin', 'last_game_date', 'birth_date'], axis=1) # drop unnecessary columns
+        print(f"----> success! {total} depositors in total.")
+        break
+    else:
+        print(f"---- failed: {r.status_code}")
+        print(f"---- {r.text}")
+        if n == 4: exit("---------------\nFailed getting depositors 5 times.\nPlease check certificates and internet connection.\n---------------")
+        print(f"-- get depositors attempt {n+1}...")
 
 for index, row in df1.iterrows(): # iterate through dataframe rows [depositors]
     # define login
+    current = index + 1 # to keep track
     login = row['login']
     player_class = row['player_class']
-    # eligible column
+    # eligibility column
     df1.at[index, 'eligible'] = True
+    df1.at[index, 'failed'] = False
     
+    print("------------------------------")
+    print(f"[{current}/{total}] {login}")
+
     # check eligibility
     if row['account_status'] == 'DEACTIVED':
-        print(f"{login} is deactivated.")
+        print(f"[{current}/{total}] {login} ineligible: deactivated.")
         df1.at[index, 'eligible'] = False
         continue
     if row['ban_status'] == 'BANNED':
-        print(f"{login} is banned.")
+        print(f"[{current}/{total}] {login} ineligible: banned.")
         df1.at[index, 'eligible'] = False
         continue
-    if player_class.casefold() == 'Bonus Abuser' or player_class.casefold() == 'Bonus Hunter':
-        print(f"{login} is an abuser.")
+    if player_class.casefold() == 'Bonus Abuser' or player_class.casefold() == 'Bonus Hunter' or player_class.casefold() == 'New'.casefold():
+        if player_class == "New": player_class = "Novice Customer" # for better reporting
+        print(f"[{current}/{total}] {login} ineligible: {player_class}.")
         df1.at[index, 'eligible'] = False
-        continue
-    if player_class.casefold() == 'New'.casefold():
-        print(f"{login} not VIP yet.")
-        df1.at[index, 'eligible'] = False
-        continue        
+        continue       
     
     # get playerID
-    r = requests.get(url=main_url+ "api/accounts/playerid", params={'login':login}, cert=cert_path)
-    pid = r.json().rstrip()
+    r = get("api/accounts/playerid",{'login':login},"player id")
+    if r == False: #failed
+        print(f"[{current}/{total}] {login} HARD FAIL (Get Player ID).") # what to do with customers who failed?
+        df1.at[index, 'failed'] = True
+        continue # next customer
+    else: pid = r.rstrip() # success
     
-    # get player finance report
-    r = requests.get(url=main_url+f"/api/players/{pid}/balance-summary", params={'startDate':start_date, 'endDate':end_date}, cert=cert_path)
-    deposits = r.json()[0]['real_deposits_amount']
-    withdrawals = r.json()[0]['real_withdrawals_amount']
-    loss = deposits - withdrawals # losses
-    ratio = withdrawals/deposits # ratio of withdrawals to deposits
-    
-    df1.at[index, 'deposits'] = deposits
-    df1.at[index, 'withdrawals'] = withdrawals
-    df1.at[index, 'loss'] = loss
+    # get player balance summary
+    r = get(f"/api/players/{pid}/balance-summary",{'startDate':start_date, 'endDate':end_date},"balance summary")
+    if r == False: # failed
+        print(f"[{current}/{total}] {login} HARD FAIL (Get Balance Sumamry).") # what to do with customers who failed?
+        df1.at[index, 'failed'] = True
+        continue # next customer
+    else: # success
+        deposits = r[0]['real_deposits_amount']
+        withdrawals = r[0]['real_withdrawals_amount']
+        loss = deposits - withdrawals # losses
+        ratio = withdrawals/deposits # ratio of withdrawals to deposits
+        df1.at[index, 'deposits'] = deposits
+        df1.at[index, 'withdrawals'] = withdrawals
+        df1.at[index, 'loss'] = loss
     
     # check if terms are met
     if deposits < min_deposits:
-        print(f'{login} ineligible - deposits below minimum [${min_deposits}].')
+        print(f'[{current}/{total}] {login} ineligible - deposits [${deposits}] below minimum [${min_deposits}].')
         df1.at[index, 'eligible'] = False
         continue
     if loss < min_loss:
-        print(f'{login} ineligible - loss below minimum [${min_loss}].')
+        print(f'[{current}/{total}] {login} ineligible - loss [${loss}] below minimum [${min_loss}].')
         df1.at[index, 'eligible'] = False
         continue
-    if ratio * 100 >= max_ratio:
-        print(f'{login} ineligible - withdrawls ratio above maximum [{max_ratio}%].')
+    if ratio * 100 > max_ratio:
+        print(f'[{current}/{total}] {login} ineligible - withdrawals ratio [{ratio}%] above maximum [{max_ratio}%].')
         df1.at[index, 'eligible'] = False
         continue
     
@@ -102,12 +196,35 @@ for index, row in df1.iterrows(): # iterate through dataframe rows [depositors]
     elif player_class.casefold() == 'Star'.casefold(): reward = 0.05 * loss
     elif player_class.casefold() == 'Prestige'.casefold(): reward = 0.08 * loss
     elif player_class.casefold() == 'Hall of Fame'.casefold(): reward = 0.1 * loss
-
-    if reward < min_reward:
-        print(f'{login} ineligible - reward below minimum [${min_reward}].')
+    df1.at[index, 'reward'] = reward # record reward
+    if reward < min_reward: # check reward vs terms
+        print(f'[{current}/{total}] {login} ineligible - reward [${reward}] below minimum [${min_reward}].')
         df1.at[index, 'eligible'] = False
         continue
+    
+    if testing: # skip to next customer
+        print("-- skip redemption and sending email [test mode is ON]")
+        continue
 
-    df1.at[index, 'reward'] = reward
+    coupon_code = create_coupon(login, reward) # try creating coupon
+    if coupon_code == False: # if failed, put Event Log Comment and send email
+        print(f"-- SOFT FAIL (Create Coupon).")
+        df1.at[index, 'processed'] = False
+        place_comment()
+        send_email(2)
+    else: # if coupon was created 
+        r = redeem_coupon(pid, coupon_code)
+        if r == False: # if excluded from coupons
+            df1.at[index, 'eligible'] = False
+            print(f"[{current}/{total}] {login} excluded from redeeming coupons.")
+            continue
+        elif r == 0: # if soft failure
+            print(f"-- SOFT FAIL (Redeem Coupon).")
+            df1.at[index, 'processed'] = False
+            place_comment()
+            send_email(2)
+        elif r: 
+            df1.at[index, 'processed'] = True
+            send_email(1)
 
 df1.to_csv('data.csv')
